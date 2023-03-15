7ca9a263b2653f0d2a42e80d4615752201e3ea31
Shawn Cong <shawn.cong@thetradedesk.com>
2023-03-15 15:21:48 -0700

    Revert "Merge pull request #105 from scong-ttd/shc-UID2-776-identity-map-optout"
    This reverts commit a735c8747f77383ed58e521d033916d70e0fc847, reversing
    changes made to 68b35b71dbb0b7c06eed7503f04b3615c0f23cf0.

--- src/test/java/com/uid2/operator/benchmark/BenchmarkRunner.java
+++
@@ -1,7 +0,0 @@
-package com.uid2.operator.benchmark;
-
-public class BenchmarkRunner {
-    public static void main(String[] args) throws Exception {
-        org.openjdk.jmh.Main.main(args);
-    }
-}

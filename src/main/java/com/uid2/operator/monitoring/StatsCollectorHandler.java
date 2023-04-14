2d36804046f08265045bf62166728e8507e02bc9
Gian Miguel Del Mundo <gianmiguel.delmundo@thetradedesk.com>
2023-04-14 14:49:25 +0800

    Fixed formatting

--- src/main/java/com/uid2/operator/monitoring/StatsCollectorHandler.java
+++ src/main/java/com/uid2/operator/monitoring/StatsCollectorHandler.java
@@ -1,31 +1,23 @@
 package com.uid2.operator.monitoring;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.uid2.operator.model.StatsCollectorMessageItem;
 import com.uid2.shared.auth.ClientKey;
 import com.uid2.shared.middleware.AuthMiddleware;
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.ext.web.RoutingContext;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class StatsCollectorHandler implements Handler<RoutingContext> {
-    private static final Logger LOGGER = LoggerFactory.getLogger(StatsCollectorHandler.class);
     private final IStatsCollectorQueue _statCollectorQueue;
     private final Vertx vertx;
 
-    private final ObjectMapper mapper;
-
-    public StatsCollectorHandler(IStatsCollectorQueue _statsQueue, Vertx vert) {
-        _statCollectorQueue = _statsQueue;
-        vertx = vert;
-        mapper = new ObjectMapper();
+    public StatsCollectorHandler(IStatsCollectorQueue _statCollectorQueue, Vertx vertx) {
+        this._statCollectorQueue = _statCollectorQueue;
+        this.vertx = vertx;
     }
 
     @Override
     public void handle(RoutingContext routingContext) {
-
         routingContext.next();
         assert routingContext != null;
 

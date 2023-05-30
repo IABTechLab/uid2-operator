b02aabbf834661f04bb7cb7e223ca1acffcd0ce1
jon8787 <112368577+jon8787@users.noreply.github.com>
2023-05-30 13:32:03 +1000

    advertising token v4 support (#135)

---
+++ src/main/java/com/uid2/operator/service/Uid2Base64UrlCoder.java
@@ -0,0 +1,43 @@
+package com.uid2.operator.service;
+
+//Copied from https://github.com/IABTechLab/uid2-client-java/blob/master/src/main/java/com/uid2/client/Uid2Base64UrlCoder.java
+
+import java.util.Base64;
+
+public class Uid2Base64UrlCoder {
+    //always use this interface to encode/decode Base64URL standard with no padding
+    //as specified on https://www.rfc-editor.org/rfc/rfc4648#section-5
+    //as unit test assumes that we are testing the encoding/decoding lib used here
+    public static String encode(byte[] bytes)
+    {
+        String encoded = Base64.getUrlEncoder().encodeToString(bytes);
+        int paddingCount = 0;
+        int len = encoded.length();
+        for(int i = 0; i < 3; i++)
+        {
+            if(len - 1 - i < 0)
+            {
+                break;
+            }
+            if(encoded.charAt(len - 1 - i) == '=')
+            {
+                paddingCount++;
+            }
+            else //'=' has to be at the very back
+            {
+                break;
+            }
+        }
+        if(paddingCount > 0)
+        {
+            //need to remove '=' padding to make it more URL friendly
+            return encoded.substring(0, len - paddingCount);
+        }
+        return encoded;
+    }
+
+    public static byte[] decode(String str)
+    {
+        return Base64.getUrlDecoder().decode(str);
+    }
+}
--- src/test/java/com/uid2/operator/TokenEncodingTest.java
+++ src/test/java/com/uid2/operator/TokenEncodingTest.java
@@ -3,6 +3,7 @@ package com.uid2.operator;
 import com.uid2.operator.model.*;
 import com.uid2.operator.service.EncodingUtils;
 import com.uid2.operator.service.EncryptedTokenEncoder;
+import com.uid2.operator.service.TokenUtils;
 import com.uid2.shared.model.EncryptionKey;
 import com.uid2.shared.store.CloudPath;
 import com.uid2.shared.store.IKeyStore;
@@ -12,10 +13,12 @@ import com.uid2.shared.store.scope.GlobalScope;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.core.json.JsonObject;
 import org.junit.Assert;
-import org.junit.Test;
-
-import java.nio.charset.StandardCharsets;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 import java.time.Instant;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TokenEncodingTest {
 
@@ -32,62 +35,75 @@ public class TokenEncodingTest {
         this.keyStoreInstance = keyStore;
     }
 
-    @Test
-    public void testRefreshTokenEncodingV2() {
+    @ParameterizedTest
+    @EnumSource(TokenVersion.class)
+    public void testRefreshTokenEncoding(TokenVersion tokenVersion) {
         final EncryptedTokenEncoder encoder = new EncryptedTokenEncoder(keyStoreInstance);
         final Instant now = EncodingUtils.NowUTCMillis();
-        final RefreshToken token = new RefreshToken(TokenVersion.V2,
+
+        final byte[] firstLevelHash = TokenUtils.getFirstLevelHashFromIdentity("test@example.com", "some-salt");
+
+        final RefreshToken token = new RefreshToken(tokenVersion,
             now,
             now.plusSeconds(360),
             new OperatorIdentity(101, OperatorType.Service, 102, 103),
             new PublisherIdentity(111, 112, 113),
-            new UserIdentity(IdentityScope.UID2, IdentityType.Email, "some-id".getBytes(StandardCharsets.UTF_8), 121, now, now.minusSeconds(122))
+            new UserIdentity(IdentityScope.UID2, IdentityType.Email, firstLevelHash, 121, now, now.minusSeconds(122))
         );
 
+        if (tokenVersion == TokenVersion.V4) {
+            Assert.assertThrows(Exception.class, () -> encoder.encode(token, now));
+            return; //V4 not supported for RefreshTokens
+        }
         final byte[] encodedBytes = encoder.encode(token, now);
         final RefreshToken decoded = encoder.decodeRefreshToken(EncodingUtils.toBase64String(encodedBytes));
 
-        Assert.assertEquals(TokenVersion.V2, decoded.version);
-        Assert.assertEquals(token.createdAt, decoded.createdAt);
-        Assert.assertEquals(token.expiresAt.plusSeconds(60), decoded.expiresAt);
-        Assert.assertTrue(token.userIdentity.matches(decoded.userIdentity));
-        Assert.assertEquals(token.userIdentity.privacyBits, decoded.userIdentity.privacyBits);
-        Assert.assertEquals(token.userIdentity.establishedAt, decoded.userIdentity.establishedAt);
-        Assert.assertEquals(token.publisherIdentity.siteId, decoded.publisherIdentity.siteId);
+        assertEquals(tokenVersion, decoded.version);
+        assertEquals(token.createdAt, decoded.createdAt);
+        int addSeconds = (tokenVersion == TokenVersion.V2) ? 60 : 0; //todo: why is there a 60 second buffer in encodeV2() but not in encodeV3()?
+        assertEquals(token.expiresAt.plusSeconds(addSeconds), decoded.expiresAt);
+        assertTrue(token.userIdentity.matches(decoded.userIdentity));
+        assertEquals(token.userIdentity.privacyBits, decoded.userIdentity.privacyBits);
+        assertEquals(token.userIdentity.establishedAt, decoded.userIdentity.establishedAt);
+        assertEquals(token.publisherIdentity.siteId, decoded.publisherIdentity.siteId);
 
         Buffer b = Buffer.buffer(encodedBytes);
-        int keyId = b.getInt(25);
+        int keyId = b.getInt(tokenVersion == TokenVersion.V2 ? 25 : 2);
         EncryptionKey key = this.keyStoreInstance.getSnapshot().getKey(keyId);
-        Assert.assertEquals(Const.Data.RefreshKeySiteId, key.getSiteId());
+        assertEquals(Const.Data.RefreshKeySiteId, key.getSiteId());
     }
 
-    @Test
-    public void testAdvertisingTokenEncodingV2() {
+    @ParameterizedTest
+    @EnumSource(TokenVersion.class)
+    public void testAdvertisingTokenEncodings(TokenVersion tokenVersion) {
         final EncryptedTokenEncoder encoder = new EncryptedTokenEncoder(keyStoreInstance);
         final Instant now = EncodingUtils.NowUTCMillis();
+
+        final byte[] rawUid = UIDOperatorVerticleTest.getRawUid(IdentityType.Email, "test@example.com", IdentityScope.UID2, tokenVersion != TokenVersion.V2);
+
         final AdvertisingToken token = new AdvertisingToken(
-            TokenVersion.V2,
+            tokenVersion,
             now,
             now.plusSeconds(60),
             new OperatorIdentity(101, OperatorType.Service, 102, 103),
             new PublisherIdentity(111, 112, 113),
-            new UserIdentity(IdentityScope.UID2, IdentityType.Email, "some-id".getBytes(StandardCharsets.UTF_8), 121, now, now.minusSeconds(122))
+            new UserIdentity(IdentityScope.UID2, IdentityType.Email, rawUid, 121, now, now.minusSeconds(122))
         );
 
         final byte[] encodedBytes = encoder.encode(token, now);
-        final AdvertisingToken decoded = encoder.decodeAdvertisingToken(EncodingUtils.toBase64String(encodedBytes));
+        final AdvertisingToken decoded = encoder.decodeAdvertisingToken(EncryptedTokenEncoder.bytesToBase64Token(encodedBytes, tokenVersion));
 
-        Assert.assertEquals(TokenVersion.V2, decoded.version);
-        Assert.assertEquals(token.createdAt, decoded.createdAt);
-        Assert.assertEquals(token.expiresAt, decoded.expiresAt);
-        Assert.assertTrue(token.userIdentity.matches(decoded.userIdentity));
-        Assert.assertEquals(token.userIdentity.privacyBits, decoded.userIdentity.privacyBits);
-        Assert.assertEquals(token.userIdentity.establishedAt, decoded.userIdentity.establishedAt);
-        Assert.assertEquals(token.publisherIdentity.siteId, decoded.publisherIdentity.siteId);
+        assertEquals(tokenVersion, decoded.version);
+        assertEquals(token.createdAt, decoded.createdAt);
+        assertEquals(token.expiresAt, decoded.expiresAt);
+        assertTrue(token.userIdentity.matches(decoded.userIdentity));
+        assertEquals(token.userIdentity.privacyBits, decoded.userIdentity.privacyBits);
+        assertEquals(token.userIdentity.establishedAt, decoded.userIdentity.establishedAt);
+        assertEquals(token.publisherIdentity.siteId, decoded.publisherIdentity.siteId);
 
         Buffer b = Buffer.buffer(encodedBytes);
-        int keyId = b.getInt(1);
+        int keyId = b.getInt(tokenVersion == TokenVersion.V2 ? 1 : 2); //TODO - extract master key from token should be a helper function
         EncryptionKey key = this.keyStoreInstance.getSnapshot().getKey(keyId);
-        Assert.assertEquals(Const.Data.MasterKeySiteId, key.getSiteId());
+        assertEquals(Const.Data.MasterKeySiteId, key.getSiteId());
     }
 }

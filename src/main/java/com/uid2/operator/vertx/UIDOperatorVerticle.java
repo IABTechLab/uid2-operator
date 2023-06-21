ad5d33b8fdd27cb0f6ea4c216a916bf644608209
Gian Miguel Del Mundo <gianmiguel.delmundo@thetradedesk.com>
2023-06-21 16:59:13 +0900

    Updated comment

--- src/main/java/com/uid2/operator/vertx/UIDOperatorVerticle.java
+++ src/main/java/com/uid2/operator/vertx/UIDOperatorVerticle.java
@@ -282,7 +282,7 @@ public class UIDOperatorVerticle extends AbstractVerticle{
             final List<EncryptionKey> keyStore = getEncryptionKeys();
 
             MissingAclMode mode = MissingAclMode.DENY_ALL;
-            // This will break if another Type is added to this map
+            // This casting will break if another Type is added to Role enum and shared lib version is not updated
             IRoleAuthorizable<Role> roleAuthorize = (IRoleAuthorizable<Role>) rc.data().get(API_CLIENT_PROP);
             if(roleAuthorize.hasRole(Role.ID_READER)) {
                 mode = MissingAclMode.ALLOW_ALL;

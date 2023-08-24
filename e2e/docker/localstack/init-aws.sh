1eba3c89b46a06fa543c3fe6049dd08d7b035c3f
Yi Shi <yi.shi@thetradedesk.com>
2023-08-24 09:48:27 +0800

    update

--- e2e/docker/localstack/init-aws.sh
+++ e2e/docker/localstack/init-aws.sh
@@ -3,8 +3,5 @@
 aws s3 --endpoint-url http://localhost:5001 mb s3://test-core-bucket
 aws s3 --endpoint-url http://localhost:5001 cp /s3/core/ s3://test-core-bucket/ --recursive
 
-aws s3 --endpoint-url http://localhost:5001 mb s3://test-snowflake-bucket
-aws s3 --endpoint-url http://localhost:5001 cp /s3/snowflake/ s3://test-snowflake-bucket/ --recursive
-
 aws s3 --endpoint-url http://localhost:5001 mb s3://test-optout-bucket
 aws s3 --endpoint-url http://localhost:5001 cp /s3/optout/ s3://test-optout-bucket/ --recursive
--- e2e/prepare_gcp_enclave_metadata.sh
+++ e2e/prepare_gcp_enclave_metadata.sh
@@ -6,7 +6,8 @@ fi
 
 # generate enclave id
 enclave_str="V1,true,$IMAGE_HASH"
-enclave_id=$(echo -n $full | openssl dgst -sha256 -binary | openssl base64)
+echo "enclave_str=$enclave_str"
+enclave_id=$(echo -n $enclave_str | openssl dgst -sha256 -binary | openssl base64)
 
 METADATA_ROOT="./e2e/docker/localstack/s3/core"
 
@@ -19,5 +20,7 @@ ENCLAVE_FILE="$METADATA_ROOT/enclaves/enclaves.json"
 cat <<< $(jq '(.[] | select(.protocol=="gcp-oidc") | .identifier) |='\"$enclave_id\"'' $ENCLAVE_FILE) > $ENCLAVE_FILE
 
 # export to Github output
+echo "OPERATOR_KEY=$operator_key"
+echo "ENCLAVE_ID=$enclave_id"
 echo "OPERATOR_KEY=$operator_key" >> $GITHUB_OUTPUT
 echo "ENCLAVE_ID=$enclave_id" >> $GITHUB_OUTPUT

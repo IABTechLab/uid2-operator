d8061a94c4a979d8ff7a1f79b96b268f2c4cb3b5
asloob qureshi <asloob.qureshi@thetradedesk.com>
2023-08-25 11:58:13 -0700

    Remove unnecessary change

--- src/main/java/com/uid2/operator/Const.java
+++ src/main/java/com/uid2/operator/Const.java
@@ -14,7 +14,5 @@ public class Const extends com.uid2.shared.Const {
         public static final String FailureShutdownWaitHoursProp = "failure_shutdown_wait_hours";
         public static final String AllowLegacyAPIProp = "allow_legacy_api";
         public static final String SharingTokenExpiryProp = "sharing_token_expiry_seconds";
-
-        public static final String CheckServiceLinkIdForIdentityMap = "check_service_link_id_for_identity_map";
     }
 }

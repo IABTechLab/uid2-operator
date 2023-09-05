930e1554ff0a4c94410a23e3fc78c8754dd7b337
ian.nara <ian.nara@thetradedesk.com>
2023-09-05 17:39:33 -0600

    fix config problem

--- src/test/java/com/uid2/operator/ServiceLinkStoreTest.java
+++ src/test/java/com/uid2/operator/ServiceLinkStoreTest.java
@@ -1,22 +1,34 @@
 package com.uid2.operator;
 
 import com.uid2.shared.cloud.EmbeddedResourceStorage;
+import com.uid2.shared.model.ServiceLink;
 import com.uid2.shared.store.CloudPath;
 import com.uid2.shared.store.reader.RotatingServiceLinkStore;
 import com.uid2.shared.store.scope.GlobalScope;
 import io.vertx.core.json.JsonObject;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ServiceLinkStoreTest {
     @Test
     public void loadFromEmbeddedResourceStorage() throws Exception {
-        RotatingServiceLinkStore fileProvider = new RotatingServiceLinkStore(
+        RotatingServiceLinkStore serviceLinkProvider = new RotatingServiceLinkStore(
                 new EmbeddedResourceStorage(Main.class),
                 new GlobalScope(new CloudPath("/com.uid2.core/test/service_links/metadata.json")));
 
-        JsonObject m = fileProvider.getMetadata();
-        assertDoesNotThrow(() -> fileProvider.loadContent(m));
+        JsonObject m = serviceLinkProvider.getMetadata();
+        assertDoesNotThrow(() -> serviceLinkProvider.loadContent(m));
+
+        List<ServiceLink> serviceLinks = new ArrayList<>(serviceLinkProvider.getAllServiceLinks());
+        assertEquals(2, serviceLinks.size());
+
+        ServiceLink sl1 = new ServiceLink("testId1", 1, 123, "testName1");
+        ServiceLink sl2 = new ServiceLink("testId2", 2, 123, "testName2");
+        assertTrue(serviceLinks.contains(sl1));
+        assertTrue(serviceLinks.contains(sl2));
     }
 }

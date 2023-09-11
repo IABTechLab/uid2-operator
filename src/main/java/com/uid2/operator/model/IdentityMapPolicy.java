6cfbbe516350986d2f48802d6474990b3e286809
Zaiwei Du <zaiwei.du@thetradedesk.com>
2023-09-11 13:58:10 -0700

    Enforce opt-out policy check

--- src/main/java/com/uid2/operator/model/IdentityMapPolicy.java
+++ src/main/java/com/uid2/operator/model/IdentityMapPolicy.java
@@ -19,4 +19,8 @@ public enum IdentityMapPolicy {
     public static com.uid2.operator.model.IdentityMapPolicy defaultPolicy() {
         return JustMap;
     }
+
+    public static IdentityMapPolicy respectOptOut() {
+        return RespectOptOut;
+    }
 }

7ac0b0a4df10624b7aeb06cf3529961405ff9565
Yi Shi （施毅） <yi.shi@thetradedesk.com>
2023-09-13 18:09:05 +0800

    fix format

--- scripts/gcp-oidc/terraform/variables.tf
+++ scripts/gcp-oidc/terraform/variables.tf
@@ -8,12 +8,12 @@ variable "region"{
 }
 
 variable "network_name" {
-  type        = string
-  default     = "uid-operator"
+    type        = string
+	default     = "uid-operator"
 }
 
 variable "service_account" {
-  type        = string
+	type        = string
 }
 
 variable "uid_machine_type"{
@@ -36,16 +36,16 @@ variable "uid_api_token"{
 
 variable "max_replicas" {
 	type        = number
-    default     = 5
+	default     = 5
 }
 
 variable "min_replicas" {
 	type        = number
-    default     = 1
+	default     = 1
 }
 
 variable "debug_mode" {
 	type        = bool
-    default     = false
+	default     = false
 }
 

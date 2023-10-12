3eb789b9ca296bc02b6fb626cfac81a289d77cf7
Lun Wang <lun.wang@thetradedesk.com>
2023-10-12 17:35:13 +0800

    Archive Azure artifacts

--- scripts/azure-cc/generate-deployment-artifacts.sh
+++ scripts/azure-cc/generate-deployment-artifacts.sh
@@ -1,13 +1,45 @@
 #!/usr/bin/env bash
 set -x
 
+if [ -z $IMAGE ]; then
+  echo "IMAGE cannot be empty"
+  exit 1
+fi
+
+if [ -z $INPUT_TEMPLATE_FILE -o ! -f $INPUT_TEMPLATE_FILE]; then
+  echo "INPUT_TEMPLATE_FILE is empty or not exist"
+  exit 1
+fi
+
 # Install Azure cli
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+if [ $? -ne 0 ]; then
+  echo "Failed to install Azure cli"
+  exit 1
+fi
 
 # Install confcom extension
 az extension add --name confcom
-sudo usermod -aG docker $USER
+if [ $? -ne 0 ]; then
+  echo "Failed to install Azure confcom extension"
+  exit 1
+fi
+
+sudo usermod -aG docker $USER # required by confcom
+if [ $? -ne 0 ]; then
+  echo "Failed to add current user to docker group"
+  exit 1
+fi
 
 # Generate deployment template
 sed "s#IMAGE_PLACEHOLDER#${IMAGE}#g" ${INPUT_TEMPLATE_FILE} > ${OUTPUT_TEMPLATE_FILE}
+if [ $? -ne 0 ]; then
+  echo "Failed to pre-process template file"
+  exit 1
+fi
+
 az confcom acipolicygen --approve-wildcards --template-file ${OUTPUT_TEMPLATE_FILE}
+if [ $? -ne 0 ]; then
+  echo "Failed to generate template file"
+  exit 1
+fi

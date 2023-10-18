eb066e9659ab7ad7fc97b83405f5b82c6464fd99
Yi Shi <yi.shi@thetradedesk.com>
2023-10-18 17:26:43 +0800

    log parameters

--- e2e/start_azure_cc_enclave.sh
+++ e2e/start_azure_cc_enclave.sh
@@ -58,11 +58,13 @@ cat "$OUTPUT_PARAMETERS_FILE" \
 | jq '(.parameters.optoutBaseUrl.value) |='\""$NGROK_URL_OPTOUT"\"'' \
 | tee "$OUTPUT_PARAMETERS_FILE"
 
+cat "$OUTPUT_PARAMETERS_FILE"
+
 az deployment group create \
     -g $RESOURCE_GROUP \
     -n $DEPLOYMENT_NAME \
     --template-file "$OUTPUT_TEMPLATE_FILE"  \
-    --parameters "$OUTPUT_PARAMETERS_FILE"
+    --parameters @"$OUTPUT_PARAMETERS_FILE"
 
 # export to Github output
 echo "CONTAINER_GROUP_NAME=$CONTAINER_GROUP_NAME"

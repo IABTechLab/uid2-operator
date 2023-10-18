46a5ec85a548efc76bed93d537ae356bfb55be16
Yi Shi <yi.shi@thetradedesk.com>
2023-10-18 21:03:35 +0800

    add comments

--- e2e/jq_helper.sh
+++ e2e/jq_helper.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# for string
+# https://jqlang.github.io/jq/manual/
+# --arg foo 123 will bind $foo to "123".
 function jq_inplace_update() {
     local file=$1
     local field=$2
@@ -7,6 +10,9 @@ function jq_inplace_update() {
     jq --arg v "$value" ".$field = \$v" "$file" > tmp.json && mv tmp.json "$file"
 }
 
+# for number/boolean
+# https://jqlang.github.io/jq/manual/
+# --argjson foo 123 will bind $foo to 123.
 function jq_inplace_update_json() {
     local file=$1
     local field=$2

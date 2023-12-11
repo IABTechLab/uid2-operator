6e5abf0098565ee4dd231e7a727c8487ad706d72
Katherine Chen <katherine.chen@thetradedesk.com>
2023-12-11 12:17:19 +1100

    Rearrange core and optout conf files

--- e2e/docker-build-public.sh
+++ e2e/docker-build-public.sh
@@ -1,10 +1,30 @@
 set -ex
 
+# Prepare conf files
+
 ROOT="."
-CORE_CONFIG_FILE="$ROOT/docker/conf/local-e2e-docker-public-config.json"
-OPTOUT_CONFIG_FILE="$ROOT/docker/conf/local-e2e-docker-public-config.json"
+CORE_CONFIG_FILE_DIR="$ROOT/docker/uid2-core/conf"
+OPTOUT_CONFIG_FILE_DIR="$ROOT/docker/uid2-optout/conf"
+
+if [ -z "$CORE_ROOT" ]; then
+  echo "CORE_ROOT can not be empty"
+  exit 1
+fi
+
+if [ -z "$OPTOUT_ROOT" ]; then
+  echo "$OPTOUT_ROOT can not be empty"
+  exit 1
+fi
+
+mkdir -p "$CORE_CONFIG_FILE_DIR" && cp "$CORE_ROOT/conf/local-e2e-docker-public-config.json" "$CORE_CONFIG_FILE_DIR"
+mkdir -p "$OPTOUT_CONFIG_FILE_DIR" && cp "$OPTOUT_ROOT/conf/local-e2e-docker-public-config.json" "$OPTOUT_CONFIG_FILE_DIR"
+
+
+CORE_CONFIG_FILE="$ROOT/docker/uid2-core/conf/local-e2e-docker-public-config.json"
+OPTOUT_CONFIG_FILE="$ROOT/docker/uid2-optout/conf/local-e2e-docker-public-config.json"
 COMPOSE_FILE="$ROOT/docker-compose.yml"
 OPTOUT_MOUNT="$ROOT/docker/uid2-optout/mount"
+
 
 source "$ROOT/jq_helper.sh"
 source "$ROOT/healthcheck.sh"

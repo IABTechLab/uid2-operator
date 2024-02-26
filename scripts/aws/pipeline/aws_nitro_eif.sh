c3335eeac31e159b3fa5703ad35a5d7510613a60
Gian Miguel Del Mundo <gianmiguel.delmundo@thetradedesk.com>
2024-02-26 14:37:10 +0800

    Added check to see if Docker is already running in amazonlinux

--- scripts/aws/pipeline/aws_nitro_eif.sh
+++ scripts/aws/pipeline/aws_nitro_eif.sh
@@ -2,22 +2,6 @@
 
 set -x
 
-cd /
-
-# Build dante
-wget https://www.inet.no/dante/files/dante-1.4.3.tar.gz
-echo "418a065fe1a4b8ace8fbf77c2da269a98f376e7115902e76cda7e741e4846a5d dante-1.4.3.tar.gz" > dante_checksum
-sha256sum --check dante_checksum
-tar -xf dante-1.4.3.tar.gz
-cd dante-1.4.3; ./configure; make; cd ..
-cp dante-1.4.3/sockd/sockd ./
-
-# Build vsockpx
-git clone https://github.com/IABTechLab/uid2-aws-enclave-vsockproxy.git
-mkdir uid2-aws-enclave-vsockproxy/build
-cd uid2-aws-enclave-vsockproxy/build; cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo; make; cd ../..
-cp uid2-aws-enclave-vsockproxy/build/vsock-bridge/src/vsock-bridge ./vsockpx
-
 # Build EIF
 dockerd &
 while (! docker stats --no-stream >/dev/null 2>&1); do

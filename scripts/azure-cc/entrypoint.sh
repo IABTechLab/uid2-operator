6416f063b721f92ab8a3293b76091a81da410db0
Thomas Manson <thomas.manson@thetradedesk.com>
2024-05-07 16:54:39 +1000

    Add a delay to get the kv

--- scripts/azure-cc/entrypoint.sh
+++ scripts/azure-cc/entrypoint.sh
@@ -4,7 +4,7 @@
 
 function wait_for_sidecar() {
   url="http://169.254.169.254/ping"
-  delay=1
+  delay=0
   max_retries=15
 
   while true; do
@@ -21,6 +21,55 @@ function wait_for_sidecar() {
       delay=$((delay + 1))
     fi
   done
+}
+
+get_key_vault_secret() {
+
+  while true; do
+    # Get the access token from IMDS
+    response=$(wget -q -O - --header="Metadata: true" "http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https://vault.azure.net")
+    
+    # Check if we got a response
+    if [ -z "$response" ]; then
+      echo "Failed to obtain access token. Retrying..."
+      sleep 1
+      continue
+    fi
+
+    # Extract the access token from the response
+    access_token=$(echo $response | jq -r '.access_token')
+
+    # Check if we got an access token
+    if [ -z "$access_token" ]; then
+      echo "Failed to parse access token. Retrying..."
+      sleep 1
+      continue
+    fi
+
+    # Use the access token to call the Key Vault service
+    secret_response=$(wget -q -O - --header="Authorization: Bearer $access_token" "https://${VAULT_NAME}.vault.azure.net/secrets/$export azure_secret_name=${OPERATOR_KEY_SECRET_NAME}?api-version=7.0")
+    
+    # Check if we got a secret response
+    if [ -z "$secret_response" ]; then
+      echo "Failed to retrieve secret. Retrying..."
+      sleep 1
+      continue
+    fi
+
+    # Extract the secret value from the response
+    secret_value=$(echo $secret_response | jq -r '.value')
+
+    # Check if we got a secret value
+    if [ -z "$secret_value" ]; then
+      echo "Failed to parse secret value. Retrying..."
+      sleep 1
+      continue
+    fi
+
+    echo "Secret Value: $secret_value"
+    break
+  done
+
 }
 
 TMP_FINAL_CONFIG="/tmp/final-config.tmp"
@@ -75,6 +124,7 @@ cat $FINAL_CONFIG
 
 # delay the start of the operator until the side car has started correctly
 wait_for_sidecar
+get_key_vault_secret
 
 # -- start operator
 echo "-- starting java application"

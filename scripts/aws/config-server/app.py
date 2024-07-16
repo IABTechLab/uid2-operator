0289d6260d7070885e42c33b6f060130e7af1eaf
Katherine Chen <katherine.chen@thetradedesk.com>
2024-07-16 15:37:17 +1000

    Add scripts/aws/config-server/

---
+++ scripts/aws/config-server/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/getConfig', methods=['GET'])
+def get_config():
+    # TODO: Figure out how to get kube secrets from k8 in python
+    return "kat-test"
+
+if __name__ == '__main__':
+    app.run(processes=8)

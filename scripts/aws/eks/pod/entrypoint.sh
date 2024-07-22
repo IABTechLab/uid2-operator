e4007e0f47300904f6b96c8c6518e359ef203ab2
Katherine Chen <katherine.chen@thetradedesk.com>
2024-07-22 16:01:21 +1000

    UID2-3702 Add python config server to eks pipeline (#739)
    * Add config server changes
    
    * Use custom branch for testing
    
    * Add scripts/aws/config-server/
    
    * Install python3.11 and pip3
    
    * Update requirements.txt path
    
    * Add debugging message for entrypoint.sh
    
    * Comment out everything in entrypoint for debugging
    
    * Update sockd conf external host name
    
    * Add scripts/aws/eks/pod/sockd_eks.conf
    
    * Install amazon-ec2-net-utils
    
    * Try out different ethnert pods
    
    * Add ip link show to debug
    
    * Add packages in Dockerfile
    
    * Install systemd-networkd
    
    * Start systemd before starting the enclave
    
    * Try eth0@if180 and debug with networkctl
    
    * Use eth0 for dante external
    
    * Add ec2-user as user
    
    * Add ec2-user user
    
    * cd into /home/config-server/ before running the flask server
    
    * Add flask app files
    
    * Provide config server the correct path
    
    * Provide config server correct path
    
    * Fix typo and provide the correct path for secret
    
    * Add .pyc files to gitignore
    
    * Remove unnecessary packages
    
    * Install net-tools to run ifconfig
    
    * Remove `EXPOSE 27015`
    
    * Remove aws-nitro-enclaves-cli-devel
    
    * Revert kcc-UID2-3702-config-server to main

--- scripts/aws/eks/pod/entrypoint.sh
+++ scripts/aws/eks/pod/entrypoint.sh
@@ -5,11 +5,23 @@ MEMORY_MB=24576
 CPU_COUNT=6
 
 function terminate_old_enclave() {
+    echo "terminate_old_enclave"
     ENCLAVE_ID=$(nitro-cli describe-enclaves | jq -r ".[0].EnclaveID")
-    [ "$ENCLAVE_ID" != "null" ] && nitro-cli terminate-enclave --enclave-id ${ENCLAVE_ID}
+    if [ "$ENCLAVE_ID" != "null" ]; then
+        nitro-cli terminate-enclave --enclave-id ${ENCLAVE_ID}
+        echo "Terminated enclave with ID ${ENCLAVE_ID}"
+    else
+        echo "No running enclaves to terminate."
+    fi
+}
+
+function debug() {
+    ip link show
+    ifconfig
 }
 
 function setup_vsockproxy() {
+    echo "setup_vsockproxy"
     VSOCK_PROXY=${VSOCK_PROXY:-/home/vsockpx}
     VSOCK_CONFIG=${VSOCK_CONFIG:-/home/proxies.host.yaml}
     VSOCK_THREADS=${VSOCK_THREADS:-$(( $(nproc) * 2 )) }
@@ -20,8 +32,15 @@ function setup_vsockproxy() {
 }
 
 function setup_dante() {
+    echo "setup_dante"
     ulimit -n 1024
     /home/sockd -D
+}
+
+function run_config_server() {
+    echo "run_config_server"
+    cd /home/config-server/
+    /config-server/bin/flask run --host 127.0.0.1 --port 27015
 }
 
 function run_enclave() {
@@ -30,6 +49,8 @@ function run_enclave() {
 }
 
 terminate_old_enclave
+debug
 setup_vsockproxy
 setup_dante
+run_config_server
 run_enclave

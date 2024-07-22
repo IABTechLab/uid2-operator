00a854ff93f1a2142758cf69b17aaeef0f63ab24
Thomas Manson <thomas.manson@thetradedesk.com>
2024-07-22 15:37:28 +1000

    Initial syslog-ng install in pod

--- scripts/aws/eks/pod/entrypoint.sh
+++ scripts/aws/eks/pod/entrypoint.sh
@@ -6,7 +6,11 @@ CPU_COUNT=6
 
 function terminate_old_enclave() {
     ENCLAVE_ID=$(nitro-cli describe-enclaves | jq -r ".[0].EnclaveID")
-    [ "$ENCLAVE_ID" != "null" ] && nitro-cli terminate-enclave --enclave-id ${ENCLAVE_ID}
+    if [ "$ENCLAVE_ID" != "null" ]; then
+        nitro-cli terminate-enclave --enclave-id ${ENCLAVE_ID}
+    else
+        echo "No running enclaves"
+    fi
 }
 
 function setup_vsockproxy() {
@@ -24,12 +28,24 @@ function setup_dante() {
     /home/sockd -D
 }
 
+function start_syslog() {
+    /usr/sbin/syslog-ng --no-caps
+}
+
 function run_enclave() {
     echo "starting enclave..."
     nitro-cli run-enclave --cpu-count $CPU_COUNT --memory $MEMORY_MB --eif-path $EIF_PATH --enclave-cid $CID --enclave-name simple-eif --debug-mode --attach-console
 }
 
+echo "starting ..."
 terminate_old_enclave
-setup_vsockproxy
-setup_dante
-run_enclave
+echo "terminated old enclaves"
+
+#setup_vsockproxy
+#setup_dante
+#run_enclave
+echo "starting syslog-ng"
+start_syslog
+echo "started syslog-ng"
+
+sleep infinity

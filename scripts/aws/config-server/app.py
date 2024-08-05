a0dae811a07b013139fb91a761f733062b1191eb
ian.nara <ian.nara@thetradedesk.com>
2024-08-05 16:54:26 -0600

    undo change to config-server and update config merging command

--- scripts/aws/config-server/app.py
+++ scripts/aws/config-server/app.py
@@ -10,12 +10,7 @@ def get_config():
             secret_value = secret_file.read().strip()
         return secret_value
     except Exception as e:
-        try:
-            token = requests.put("http://169.254.169.254/latest/api/token", headers={"X-aws-ec2-metadata-token-ttl-seconds": "3600"})
-            user_data = requests.get("http://169.254.169.254/latest/user-data", headers={"X-aws-ec2-metadata-token": token.text})
-            return user_data.text
-        except Exception as e:
-            return str(e), 500
+        return str(e), 500
 
 if __name__ == '__main__':
     app.run(processes=8)

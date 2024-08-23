6429cf0d914dc0da1285d6b610f66fbdb7da0001
Cody Constine <cody.constine@thetradedesk.com>
2024-08-23 15:18:57 -0600

    Added config server makes things lower

--- scripts/aws/config-server/app.py
+++ scripts/aws/config-server/app.py
@@ -7,6 +7,9 @@ def get_config():
     try:
         with open('/etc/secret/secret-value/config', 'r') as secret_file:
             secret_value = secret_file.read().strip()
+            secret_value["environment"] = secret_value["environment"].lower()
+            secret_value["core_base_url"] = secret_value["core_base_url"].lower()
+            secret_value["optout_base_url"] = secret_value["optout_base_url"].lower()
         return secret_value
     except Exception as e:
         return str(e), 500

f7ac096d7b6c412d18da05ef4c8ddd4ee6c2dab2
Gian Miguel Del Mundo <gianmiguel.delmundo@thetradedesk.com>
2024-08-29 13:47:22 +0800

    Updated DebugRequestCapturingHandler condition

--- src/main/java/com/uid2/operator/vertx/DebugRequestCapturingHandler.java
+++ src/main/java/com/uid2/operator/vertx/DebugRequestCapturingHandler.java
@@ -1,6 +1,7 @@
 package com.uid2.operator.vertx;
 
 import com.uid2.shared.vertx.RequestCapturingHandler;
+import io.vertx.core.http.impl.HttpUtils;
 import io.vertx.ext.web.RoutingContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,11 +11,18 @@ public class DebugRequestCapturingHandler extends RequestCapturingHandler {
 
     @Override
     public void handle(RoutingContext context) {
-        String path = context.currentRoute().getPath();
         String uri = context.request().uri();
+        String path = uri;
+        try {
+            String normalized = HttpUtils.normalizePath(uri).split("\\?")[0];
+            path = Endpoints.pathSet().contains(normalized) ? normalized : "/unknown";
+        } catch (IllegalArgumentException e) {
+            path = "/unknown";
+        }
+
         int status = context.request().response().getStatusCode();
 
-        if (path == null && (status >= 200 && status < 300)) {
+        if ((path == null || path.contains("unknown")) && (status >= 200 && status < 300)) {
             LOGGER.error("Unknown path with URI [{}] has a successful 2xx HTTP code [{}]", uri, status);
         }
 

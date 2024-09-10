94aafebf34f6dd523dd8c9d6267e8faf308787b6
Katherine Chen <katherine.chen@thetradedesk.com>
2024-09-11 07:36:44 +1000

    Hide api token from log (#952)
    * Hide api token
    
    * start script api token
    
    ---------
    
    Co-authored-by: ian.nara <ian.nara@thetradedesk.com>

--- scripts/aws/start.sh
+++ scripts/aws/start.sh
@@ -95,11 +95,9 @@ function setup_dante() {
 function run_config_server() {
     mkdir -p /etc/secret/secret-value
     SECRET_JSON=$(aws secretsmanager get-secret-value --secret-id "$UID2_CONFIG_SECRET_KEY" | jq -r '.SecretString')
-    echo ${SECRET_JSON}
-    echo ${SECRET_JSON} > /etc/secret/secret-value/config
+    { set +x; } 2>/dev/null; { echo ${SECRET_JSON} > /etc/secret/secret-value/config; set -x; }
     echo $(jq ".core_base_url = \"$CORE_BASE_URL\"" /etc/secret/secret-value/config) > /etc/secret/secret-value/config
     echo $(jq ".optout_base_url = \"$OPTOUT_BASE_URL\"" /etc/secret/secret-value/config) > /etc/secret/secret-value/config
-    cat /etc/secret/secret-value/config
     echo "run_config_server"
     cd /opt/uid2operator/config-server
     ./bin/flask run --host 127.0.0.1 --port 27015 &

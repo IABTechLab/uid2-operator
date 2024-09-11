81b88f9852c4f7209a03472c0d632fdf904fe4c4
Katherine Chen <katherine.chen@thetradedesk.com>
2024-09-11 10:07:09 +1000

    Hide api token (#954)
    * Hide api token
    
    * Fix output

--- scripts/aws/start.sh
+++ scripts/aws/start.sh
@@ -94,8 +94,12 @@ function setup_dante() {
 
 function run_config_server() {
     mkdir -p /etc/secret/secret-value
-    SECRET_JSON=$(aws secretsmanager get-secret-value --secret-id "$UID2_CONFIG_SECRET_KEY" | jq -r '.SecretString')
-    { set +x; } 2>/dev/null; { echo ${SECRET_JSON} > /etc/secret/secret-value/config; set -x; }
+    { 
+        set +x;  # Disable tracing within this block
+        2>/dev/null;
+        SECRET_JSON=$(aws secretsmanager get-secret-value --secret-id "$UID2_CONFIG_SECRET_KEY" | jq -r '.SecretString')
+        echo "${SECRET_JSON}" > config; 
+    }
     echo $(jq ".core_base_url = \"$CORE_BASE_URL\"" /etc/secret/secret-value/config) > /etc/secret/secret-value/config
     echo $(jq ".optout_base_url = \"$OPTOUT_BASE_URL\"" /etc/secret/secret-value/config) > /etc/secret/secret-value/config
     echo "run_config_server"

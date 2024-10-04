b24fbf8f84ed69dee3d3c9c4128e3fd8d6ab003b
ian.nara <ian.nara@thetradedesk.com>
2024-10-04 15:38:56 -0600

    don't use logback conf in debug mode

--- scripts/aws/entrypoint.sh
+++ scripts/aws/entrypoint.sh
@@ -98,12 +98,23 @@ cd /app
 
 # -- start operator
 echo "Starting Java application..."
-java \
-  -XX:MaxRAMPercentage=95 -XX:-UseCompressedOops -XX:+PrintFlagsFinal \
-  -Djava.security.egd=file:/dev/./urandom \
-  -Djava.library.path=/app/lib \
-  -Dvertx-config-path="${FINAL_CONFIG}" \
-  -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.SLF4JLogDelegateFactory \
-  -Dlogback.configurationFile=./conf/logback.xml \
-  -Dhttp_proxy=socks5://127.0.0.1:3305 \
-  -jar /app/"${JAR_NAME}"-"${JAR_VERSION}".jar
+if [[ ! "$DEBUG_MODE" = "true" ]]; then
+  java \
+    -XX:MaxRAMPercentage=95 -XX:-UseCompressedOops -XX:+PrintFlagsFinal \
+    -Djava.security.egd=file:/dev/./urandom \
+    -Djava.library.path=/app/lib \
+    -Dvertx-config-path="${FINAL_CONFIG}" \
+    -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.SLF4JLogDelegateFactory \
+    -Dhttp_proxy=socks5://127.0.0.1:3305 \
+    -jar /app/"${JAR_NAME}"-"${JAR_VERSION}".jar
+else
+  java \
+      -XX:MaxRAMPercentage=95 -XX:-UseCompressedOops -XX:+PrintFlagsFinal \
+      -Djava.security.egd=file:/dev/./urandom \
+      -Djava.library.path=/app/lib \
+      -Dvertx-config-path="${FINAL_CONFIG}" \
+      -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.SLF4JLogDelegateFactory \
+      -Dlogback.configurationFile=./conf/logback.xml \
+      -Dhttp_proxy=socks5://127.0.0.1:3305 \
+      -jar /app/"${JAR_NAME}"-"${JAR_VERSION}".jar
+fi

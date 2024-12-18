8018196a3e5f2e3432105fb7432eb8ed740ed25d
Behnam Mozafari <behnam.mozafari@thetradedesk.com>
2024-12-18 15:47:15 +1100

    Added ConfigValidatorUtilTest, Removed unused imports in UIDOperatorVerticleTest

---
+++ src/test/java/com/uid2/operator/service/ConfigValidatorUtilTest.java
@@ -0,0 +1,32 @@
+package com.uid2.operator.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ConfigValidatorUtilTest {
+    @Test
+    void testValidateIdentityRefreshTokens() {
+        // identityExpiresAfter is greater than refreshExpiresAfter
+        assertFalse(ConfigValidatorUtil.validateIdentityRefreshTokens(10, 5, 3));
+
+        // refreshIdentityAfter is greater than identityExpiresAfter
+        assertFalse(ConfigValidatorUtil.validateIdentityRefreshTokens(5, 10, 6));
+
+        // refreshIdentityAfter is greater than refreshExpiresAfter
+        assertFalse(ConfigValidatorUtil.validateIdentityRefreshTokens(5, 10, 11));
+
+        // all conditions are valid
+        assertTrue(ConfigValidatorUtil.validateIdentityRefreshTokens(5, 10, 3));
+    }
+
+    @Test
+    void testValidateBidstreamLifetime() {
+        // maxBidstreamLifetimeSeconds is less than identityTokenExpiresAfterSeconds
+        assertFalse(ConfigValidatorUtil.validateBidstreamLifetime(5, 10));
+
+        // maxBidstreamLifetimeSeconds is greater than or equal to identityTokenExpiresAfterSeconds
+        assertTrue(ConfigValidatorUtil.validateBidstreamLifetime(10, 5));
+        assertTrue(ConfigValidatorUtil.validateBidstreamLifetime(10, 10));
+    }
+}

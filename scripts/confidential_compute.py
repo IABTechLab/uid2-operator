3301aa4cf3d57016e4d1ea16b00ab7a2b980462f
Xu Yang <58192524+clarkxuyang@users.noreply.github.com>
2025-01-23 13:47:12 -0800

    UID2-4719 change azure cc starting process (#1260)
    * UID2-4719 change azure cc starting process

--- scripts/confidential_compute.py
+++ scripts/confidential_compute.py
@@ -5,6 +5,7 @@ from urllib.parse import urlparse
 from abc import ABC, abstractmethod
 from typing import TypedDict, NotRequired, get_type_hints
 import subprocess
+import logging
 
 class ConfidentialComputeConfig(TypedDict):
     api_token: str
@@ -17,9 +18,9 @@ class ConfidentialComputeConfig(TypedDict):
 class ConfidentialComputeStartupException(Exception):
     def __init__(self, error_name, provider, extra_message=None):
         urls = {
-            "EC2": "https://unifiedid.com/docs/guides/operator-guide-aws-marketplace#uid2-operator-error-codes",
-            "Azure": "https://unifiedid.com/docs/guides/operator-guide-azure-enclave#uid2-operator-error-codes",
-            "GCPEntrypoint": "https://unifiedid.com/docs/guides/operator-private-gcp-confidential-space#uid2-operator-error-codes",
+            "EC2EntryPoint": "https://unifiedid.com/docs/guides/operator-guide-aws-marketplace#uid2-operator-error-codes",
+            "AzureEntryPoint": "https://unifiedid.com/docs/guides/operator-guide-azure-enclave#uid2-operator-error-codes",
+            "GCPEntryPoint": "https://unifiedid.com/docs/guides/operator-private-gcp-confidential-space#uid2-operator-error-codes",
         }
         url = urls.get(provider)
         super().__init__(f"{error_name}\n" + (extra_message if extra_message else "") + f"\nVisit {url} for more details")
@@ -28,7 +29,7 @@ class MissingInstanceProfile(ConfidentialComputeStartupException):
     def __init__(self, cls, message = None):
         super().__init__(error_name=f"E01: {self.__class__.__name__}", provider=cls, extra_message=message)
 
-class ConfigNotFound(ConfidentialComputeStartupException):
+class ApiTokenNotFound(ConfidentialComputeStartupException):
     def __init__(self, cls, message = None):
         super().__init__(error_name=f"E02: {self.__class__.__name__}", provider=cls, extra_message=message)
 
@@ -47,7 +48,15 @@ class InvalidOperatorKey(ConfidentialComputeStartupException):
 class UID2ServicesUnreachable(ConfidentialComputeStartupException):
     def __init__(self, cls, ip=None):
         super().__init__(error_name=f"E06: {self.__class__.__name__}", provider=cls, extra_message=ip)
-    
+
+class AuxiliariesException(ConfidentialComputeStartupException):
+    def __init__(self, cls, inner_message = None):
+        super().__init__(error_name=f"E07: {self.__class__.__name__}", provider=cls, extra_message=inner_message)
+
+class SecretAccessDenied(ConfidentialComputeStartupException):
+    def __init__(self, cls, message = None):
+        super().__init__(error_name=f"E08: {self.__class__.__name__}", provider=cls, extra_message=message)
+
 class ConfidentialCompute(ABC):
 
     def __init__(self):
@@ -55,7 +64,7 @@ class ConfidentialCompute(ABC):
 
     def validate_configuration(self):
         """ Validates the paramters specified through configs/secret manager ."""
-        print("Validating configurations provided")
+        logging.info("Validating configurations provided")
         def validate_operator_key():
             """ Validates the operator key format and its environment alignment."""
             operator_key = self.configs.get("api_token")
@@ -66,9 +75,9 @@ class ConfidentialCompute(ABC):
                 expected_env = "I" if debug_mode or env == "integ" else "P"
                 if operator_key.split("-")[2] != expected_env:
                     raise InvalidOperatorKey(self.__class__.__name__)
-                print("Validated operator key matches environment")
+                logging.info("Validated operator key matches environment")
             else:
-                print("Skipping operator key validation")
+                logging.info("Skipping operator key validation")
 
         def validate_url(url_key, environment):
             """URL should include environment except in prod"""
@@ -77,7 +86,7 @@ class ConfidentialCompute(ABC):
             parsed_url = urlparse(self.configs[url_key])
             if parsed_url.scheme != 'https' and parsed_url.path:
                 raise InvalidConfigValue(self.__class__.__name__, url_key)
-            print(f"Validated {self.configs[url_key]} matches other config parameters")
+            logging.info(f"Validated {self.configs[url_key]} matches other config parameters")
             
         def validate_connectivity() -> None:
             """ Validates that the core URL is accessible."""
@@ -85,7 +94,7 @@ class ConfidentialCompute(ABC):
                 core_url = self.configs["core_base_url"]
                 core_ip = socket.gethostbyname(urlparse(core_url).netloc)
                 requests.get(core_url, timeout=5)
-                print(f"Validated connectivity to {core_url}")
+                logging.info(f"Validated connectivity to {core_url}")
             except (requests.ConnectionError, requests.Timeout) as e:
                 raise UID2ServicesUnreachable(self.__class__.__name__, core_ip)
             except Exception as e:
@@ -108,15 +117,12 @@ class ConfidentialCompute(ABC):
         validate_url("optout_base_url", environment)
         validate_operator_key()
         validate_connectivity()
-        print("Completed static validation of confidential compute config values")
+        logging.info("Completed static validation of confidential compute config values")
         
     @abstractmethod
-    def _get_secret(self, secret_identifier: str) -> ConfidentialComputeConfig:
+    def _set_confidential_config(self, secret_identifier: str) -> None:
         """
-        Fetches the secret from a secret store.
-
-        Raises:
-            SecretNotFoundException: If the secret is not found.
+        Set ConfidentialComputeConfig
         """
         pass
 
@@ -136,13 +142,13 @@ class ConfidentialCompute(ABC):
         pass
 
     @staticmethod
-    def run_command(command, seperate_process=False):
-        print(f"Running command: {' '.join(command)}")
+    def run_command(command, separate_process=False):
+        logging.info(f"Running command: {' '.join(command)}")
         try:
-            if seperate_process:
+            if separate_process:
                 subprocess.Popen(command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
             else:
                 subprocess.run(command,check=True)
         except Exception as e:
-            print(f"Failed to run command: {str(e)}")
+            logging.error(f"Failed to run command: {e}", exc_info=True)
             raise RuntimeError (f"Failed to start {' '.join(command)} ")

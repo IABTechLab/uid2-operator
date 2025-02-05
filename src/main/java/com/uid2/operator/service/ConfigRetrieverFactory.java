bdb929b7e9166987c542bb94a99cba8c74795caa
abuabraham-ttd <130511456+abuabraham-ttd@users.noreply.github.com>
2025-02-05 13:02:16 -0800

    Update ConfigRetrieverFactory.java

--- src/main/java/com/uid2/operator/service/ConfigRetrieverFactory.java
+++ src/main/java/com/uid2/operator/service/ConfigRetrieverFactory.java
@@ -5,33 +5,29 @@ import io.vertx.config.ConfigRetrieverOptions;
 import io.vertx.config.ConfigStoreOptions;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import static com.uid2.operator.Const.Config.ConfigScanPeriodMsProp;
 
 public class ConfigRetrieverFactory {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ConfigRetrieverFactory.class);
     public static ConfigRetriever create(Vertx vertx, JsonObject bootstrapConfig, String operatorKey) {
         String type = bootstrapConfig.getString("type");
         JsonObject storeConfig = bootstrapConfig.getJsonObject("config");
         LOGGER.info("ABU operatorKey");
         LOGGER.info(operatorKey);
         if (type.equals("http")) {
-            LOGGER.info("ABU httpcall");
             storeConfig.put("headers", new JsonObject()
                     .put("Authorization", "Bearer " + operatorKey));
-            LOGGER.info("ABU header uodated");
         }
 
         ConfigStoreOptions storeOptions = new ConfigStoreOptions()
                 .setType(type)
                 .setConfig(storeConfig);
 
-        LOGGER.info("ABU storeOptions uodated");
-
         ConfigRetrieverOptions retrieverOptions = new ConfigRetrieverOptions()
                 .setScanPeriod(bootstrapConfig.getLong(ConfigScanPeriodMsProp))
                 .addStore(storeOptions);
-
-        LOGGER.info("ABU retrieverOptions uodated");
 
         return ConfigRetriever.create(vertx, retrieverOptions);
     }

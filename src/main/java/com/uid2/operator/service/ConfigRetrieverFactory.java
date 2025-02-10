255d98dfff68ec6c1a46d2e1c9c82fd2406dc9ea
Behnam Mozafari <behnam.mozafari@thetradedesk.com>
2025-02-11 09:30:17 +1100

    Change remote_config_store with http type to use single "url" field - Update ConfigRetrieverFactory to parse url into URI for http store

--- src/main/java/com/uid2/operator/service/ConfigRetrieverFactory.java
+++ src/main/java/com/uid2/operator/service/ConfigRetrieverFactory.java
@@ -6,6 +6,8 @@ import io.vertx.config.ConfigStoreOptions;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
 
+import java.net.URI;
+
 import static com.uid2.operator.Const.Config.ConfigScanPeriodMsProp;
 
 public class ConfigRetrieverFactory {
@@ -13,6 +15,12 @@ public class ConfigRetrieverFactory {
         String type = bootstrapConfig.getString("type");
         JsonObject storeConfig = bootstrapConfig.getJsonObject("config");
         if (type.equals("http")) {
+            URI uri = URI.create(storeConfig.getString("url"));
+            storeConfig.remove("url");
+            storeConfig.put("host", uri.getHost());
+            storeConfig.put("port", uri.getPort() == -1 ? (uri.getScheme().equals("https") ? 443 : 80) : uri.getPort());
+            storeConfig.put("path", uri.getPath());
+            storeConfig.put("ssl", uri.getScheme().equals("https"));
             storeConfig.put("headers", new JsonObject()
                     .put("Authorization", "Bearer " + operatorKey));
         }

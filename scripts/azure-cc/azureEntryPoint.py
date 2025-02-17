13601d5440ab2fb6723e18d5899e60c9e5130e05
Katherine Chen <katherine.chen@thetradedesk.com>
2025-02-17 18:12:53 +1100

    Add test message

--- scripts/azure-cc/azureEntryPoint.py
+++ scripts/azure-cc/azureEntryPoint.py
@@ -92,8 +92,8 @@ class AzureEntryPoint(ConfidentialCompute):
         
 
     def _set_confidential_config(self, secret_identifier: str = None):
-        self.configs["skip_validations"] = os.getenv("SKIP_VALIDATIONS", "false").lower() == "true"
-        self.configs["debug_mode"] = os.getenv("DEBUG_MODE", "false").lower() == "true"
+        self.configs["skip_validations"] = os.getenv("SKIP_VALIDATIONS", "true").lower() == "true"
+        self.configs["debug_mode"] = os.getenv("DEBUG_MODE", "true").lower() == "true"
         self.configs["environment"] = AzureEntryPoint.env_name
 
         # set self.configs["operator_key"]

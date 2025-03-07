b61597a535e6e95dac8f1a5bddb33a83b073b32b
Matt Collins <matt.collins@thetradedesk.com>
2025-03-07 15:16:54 +1100

    test scripts

---
+++ scripts/azure-vn/deployment/test.sh
@@ -0,0 +1,6 @@
+export IMAGE=ghcr.io/iabtechlab/uid2-operator:5.49.9-alpha-224-SNAPSHOT-azure-cc
+export OUTPUT_DIR=artifacts
+export MANIFEST_DIR=manifest
+export VERSION_NUMBER=5.49.9-alpha-224-SNAPSHOT-azure-cc
+
+bash ./generate-deployment-artifacts.sh

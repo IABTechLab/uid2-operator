e54832b04d8348dae85b40276b53b2372fdd8bf1
Matt Collins <matt.collins@thetradedesk.com>
2025-03-31 14:35:59 +1100

    wip

--- src/main/java/com/uid2/operator/store/ConfigStore.java
+++ src/main/java/com/uid2/operator/store/ConfigStore.java
@@ -1,12 +1,27 @@
 package com.uid2.operator.store;
 
+import com.uid2.shared.Utils;
+import com.uid2.shared.cloud.DownloadCloudStorage;
 import com.uid2.shared.store.reader.IMetadataVersionedStore;
 import io.vertx.core.json.JsonObject;
 
+import java.io.InputStream;
+
 public class ConfigStore implements IMetadataVersionedStore {
+    private final DownloadCloudStorage fileStreamProvider;
+
+    public ConfigStore(DownloadCloudStorage fileStreamProvider) {
+        this.fileStreamProvider = fileStreamProvider;
+    }
+    
     @Override
     public JsonObject getMetadata() throws Exception {
-        return null;
+        // TODO
+        try (InputStream s = this.fileStreamProvider.download("http://localhost:8088/operator/config")) {
+            return Utils.toJsonObject(s);
+        }
+//        this.fileStreamProvider.download()
+//        return null;
     }
 
     @Override

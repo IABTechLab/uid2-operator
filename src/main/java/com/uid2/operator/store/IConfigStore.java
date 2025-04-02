2b01fc4c6c5b807f5c5dfb7f562cb2dadd453b81
Katherine Chen <katherine.chen@thetradedesk.com>
2025-04-02 16:31:32 +1100

    Add getMetadata to IConfigStore

--- src/main/java/com/uid2/operator/store/IConfigStore.java
+++ src/main/java/com/uid2/operator/store/IConfigStore.java
@@ -1,6 +1,9 @@
 package com.uid2.operator.store;
 
+import io.vertx.core.json.JsonObject;
+
 public interface IConfigStore {
     RuntimeConfig getConfig();
     void loadContent() throws Exception;
+    JsonObject getMetadata() throws Exception;
 }

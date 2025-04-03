857047af35dd1544e6fd59fc692c53952fb2b961
Katherine Chen <katherine.chen@thetradedesk.com>
2025-04-03 13:26:23 +1100

    Add logging to BootstrapConfig

--- src/main/java/com/uid2/operator/store/BootstrapConfigStore.java
+++ src/main/java/com/uid2/operator/store/BootstrapConfigStore.java
@@ -1,11 +1,15 @@
 package com.uid2.operator.store;
 
 import io.vertx.core.json.JsonObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class BootstrapConfigStore implements IConfigStore {
+    private static final Logger logger = LoggerFactory.getLogger(BootstrapConfigStore.class);
     private final JsonObject config;
 
     public BootstrapConfigStore(JsonObject config) {
+        logger.info("Successfully loaded bootstrap config");
         this.config = config;
     }
 
@@ -16,11 +20,13 @@ public class BootstrapConfigStore implements IConfigStore {
 
     @Override
     public void loadContent() throws Exception {
+        logger.info("Remote Config FF is not enabled, bootstrap config was loaded.");
         return;
     }
 
     @Override
     public JsonObject getMetadata() throws Exception {
+        logger.info("Successfully loaded bootstrap config");
         return this.config;
     }
 }

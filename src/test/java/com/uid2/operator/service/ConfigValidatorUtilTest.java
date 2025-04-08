923963f1a79f53b5897e7fbfb4658c6e16410a41
Matt Collins <matt.collins@thetradedesk.com>
2025-04-08 13:47:24 +1000

    wip

--- src/test/java/com/uid2/operator/service/ConfigValidatorUtilTest.java
+++ src/test/java/com/uid2/operator/service/ConfigValidatorUtilTest.java
@@ -9,62 +9,63 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
+import static com.uid2.operator.Const.Config.SharingTokenExpiryProp;
+import static com.uid2.operator.service.UIDOperatorService.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ConfigValidatorUtilTest {
-    public static final String IDENTITY_TOKEN_EXPIRES_AFTER_SECONDS = "identity_token_expires_after_seconds";
-    public static final String REFRESH_TOKEN_EXPIRES_AFTER_SECONDS = "refresh_token_expires_after_seconds";
-    public static final String REFRESH_IDENTITY_TOKEN_AFTER_SECONDS = "refresh_identity_token_after_seconds";
-    public static final String SHARING_TOKEN_EXPIRY_SECONDS = "sharing_token_expiry_seconds";
-    
     private final JsonObject validConfig = new JsonObject()
-            .put(ConfigValidatorUtilTest.IDENTITY_TOKEN_EXPIRES_AFTER_SECONDS, 259200)
-            .put(ConfigValidatorUtilTest.REFRESH_TOKEN_EXPIRES_AFTER_SECONDS, 2592000)
-            .put(ConfigValidatorUtilTest.REFRESH_IDENTITY_TOKEN_AFTER_SECONDS, 3600)
-            .put(ConfigValidatorUtilTest.SHARING_TOKEN_EXPIRY_SECONDS,  2592000);
+            .put(IDENTITY_TOKEN_EXPIRES_AFTER_SECONDS, 259200)
+            .put(REFRESH_TOKEN_EXPIRES_AFTER_SECONDS, 2592000)
+            .put(REFRESH_IDENTITY_TOKEN_AFTER_SECONDS, 3600)
+            .put(SharingTokenExpiryProp,  2592000);
     
     @Test
     void validConfigDoesNotThrow() {
-        assertDoesNotThrow(() -> validConfig.mapTo(RuntimeConfig.class));
+        assertDoesNotThrow(this::mapToRuntimeConfig);
     }
     
     @Test
     void extraPropertyDoesNotThrow() {
         validConfig.put("some_new_property", 1);
-        assertDoesNotThrow(() -> validConfig.mapTo(RuntimeConfig.class));
+        assertDoesNotThrow(this::mapToRuntimeConfig);
     }
     
     @ParameterizedTest
     @MethodSource("requiredFields")
     void requiredFieldIsNullThrows(String propertyName) {
         validConfig.putNull(propertyName);
-        assertThrows(IllegalArgumentException.class, () -> validConfig.mapTo(RuntimeConfig.class));
+        assertThrows(IllegalArgumentException.class, this::mapToRuntimeConfig);
     }
 
     @ParameterizedTest
     @MethodSource("requiredFields")
     void requiredFieldIsMissingThrows(String propertyName) {
         validConfig.remove(propertyName);
-        assertThrows(IllegalArgumentException.class, () -> validConfig.mapTo(RuntimeConfig.class));
+        assertThrows(IllegalArgumentException.class, this::mapToRuntimeConfig);
     }
 
+    private void mapToRuntimeConfig() {
+        validConfig.mapTo(RuntimeConfig.class);
+    }
+    
     private static Stream<Arguments> requiredFields() {
         return Stream.of(
                 Arguments.of(IDENTITY_TOKEN_EXPIRES_AFTER_SECONDS),
                 Arguments.of(REFRESH_TOKEN_EXPIRES_AFTER_SECONDS),
                 Arguments.of(REFRESH_IDENTITY_TOKEN_AFTER_SECONDS),
-                Arguments.of(SHARING_TOKEN_EXPIRY_SECONDS)
+                Arguments.of(SharingTokenExpiryProp)
         );
     }
         
     @Test
     void identityTokenExpiresAfterSecondsIsGreaterThanRefreshTokenExpiresAfterSecondsThrows() {
         // identityExpiresAfter is greater than refreshExpiresAfter
-        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class, () ->
-                validConfig.put("identity_token_expires_after_seconds", 10)
-                        .put("refresh_token_expires_after_seconds", 5)
-                        .mapTo(RuntimeConfig.class)
-        );
+
+        validConfig.put(IDENTITY_TOKEN_EXPIRES_AFTER_SECONDS, 10);
+        validConfig.put(REFRESH_TOKEN_EXPIRES_AFTER_SECONDS, 5);
+
+        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class, () -> validConfig.mapTo(RuntimeConfig.class) );
     }
     
 //

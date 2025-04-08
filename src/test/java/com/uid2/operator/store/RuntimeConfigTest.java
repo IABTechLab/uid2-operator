06f74b448afb56096d52aedc160996e00bd1708f
Matt Collins <matt.collins@thetradedesk.com>
2025-04-08 14:17:00 +1000

    wip

--- src/test/java/com/uid2/operator/store/RuntimeConfigTest.java
+++ src/test/java/com/uid2/operator/store/RuntimeConfigTest.java
@@ -1,6 +1,5 @@
-package com.uid2.operator.service;
+package com.uid2.operator.store;
 
-import com.uid2.operator.store.RuntimeConfig;
 import io.vertx.core.json.JsonObject;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -9,11 +8,12 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
+import static com.uid2.operator.Const.Config.MaxBidstreamLifetimeSecondsProp;
 import static com.uid2.operator.Const.Config.SharingTokenExpiryProp;
 import static com.uid2.operator.service.UIDOperatorService.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class ConfigValidatorUtilTest {
+public class RuntimeConfigTest {
     private final JsonObject validConfig = new JsonObject()
             .put(IDENTITY_TOKEN_EXPIRES_AFTER_SECONDS, 259200)
             .put(REFRESH_TOKEN_EXPIRES_AFTER_SECONDS, 2592000)
@@ -60,48 +60,25 @@ public class ConfigValidatorUtilTest {
         
     @Test
     void identityTokenExpiresAfterSecondsIsGreaterThanRefreshTokenExpiresAfterSecondsThrows() {
-        // identityExpiresAfter is greater than refreshExpiresAfter
-
         validConfig.put(IDENTITY_TOKEN_EXPIRES_AFTER_SECONDS, 10);
         validConfig.put(REFRESH_TOKEN_EXPIRES_AFTER_SECONDS, 5);
 
-        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class, () -> validConfig.mapTo(RuntimeConfig.class) );
+        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class, this::mapToRuntimeConfig);
     }
     
-//
-//        assertFalse(ConfigValidatorUtil.validateIdentityRefreshTokens(10, 5, 3));
-//
-//        // refreshIdentityAfter is greater than identityExpiresAfter
-//        assertFalse(ConfigValidatorUtil.validateIdentityRefreshTokens(5, 10, 6));
-//
-//        // refreshIdentityAfter is greater than refreshExpiresAfter
-//        assertFalse(ConfigValidatorUtil.validateIdentityRefreshTokens(5, 10, 11));
-//
-//        // all conditions are valid
-//        assertTrue(ConfigValidatorUtil.validateIdentityRefreshTokens(5, 10, 3));
-
     @Test
-    void testValidateBidstreamLifetime() {
-        // maxBidstreamLifetimeSeconds is less than identityTokenExpiresAfterSeconds
-//        assertFalse(ConfigValidatorUtil.validateBidstreamLifetime(5, 10));
-//
-//        // maxBidstreamLifetimeSeconds is greater than or equal to identityTokenExpiresAfterSeconds
-//        assertTrue(ConfigValidatorUtil.validateBidstreamLifetime(10, 5));
-//        assertTrue(ConfigValidatorUtil.validateBidstreamLifetime(10, 10));
+    void refreshIdentityAfterSecondsIsGreaterThanIdentityTokenExpiresAfterSecondsThrows() {
+        validConfig.put(REFRESH_IDENTITY_TOKEN_AFTER_SECONDS, 6);
+        validConfig.put(IDENTITY_TOKEN_EXPIRES_AFTER_SECONDS, 5);
+
+        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class, this::mapToRuntimeConfig);
     }
-
+    
     @Test
-    void testValidateIdentityRefreshTokensWithNullValues() {
-//        // identityExpiresAfter is null
-//        assertFalse(ConfigValidatorUtil.validateIdentityRefreshTokens(null, 10, 5));
-//
-//        // refreshExpiresAfter is null
-//        assertFalse(ConfigValidatorUtil.validateIdentityRefreshTokens(10, null, 5));
-//
-//        // refreshIdentityAfter is null
-//        assertFalse(ConfigValidatorUtil.validateIdentityRefreshTokens(10, 5, null));
-//
-//        // all values are null
-//        assertFalse(ConfigValidatorUtil.validateIdentityRefreshTokens(null, null, null));
+    void maxBidStreamLifetimeSecondsIsLessThanIdentityTokenExpiresAfterSecondsThrows() {
+        validConfig.put(MaxBidstreamLifetimeSecondsProp, 5);
+        validConfig.put(IDENTITY_TOKEN_EXPIRES_AFTER_SECONDS, 10);
+        
+        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class, this::mapToRuntimeConfig);
     }
 }

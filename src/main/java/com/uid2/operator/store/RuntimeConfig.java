8fb9a033d0ab50332b9266d2b8ef41c3a95356bc
Matt Collins <matt.collins@thetradedesk.com>
2025-04-08 11:01:20 +1000

    wip

--- src/main/java/com/uid2/operator/store/RuntimeConfig.java
+++ src/main/java/com/uid2/operator/store/RuntimeConfig.java
@@ -8,7 +8,6 @@ import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
 
 @JsonDeserialize(builder = RuntimeConfig.Builder.class)
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class RuntimeConfig {
     private final Integer identityTokenExpiresAfterSeconds;
     private final Integer refreshTokenExpiresAfterSeconds;
@@ -90,15 +89,16 @@ public class RuntimeConfig {
     }
     
     private void validateBidstreamLifetime() {
-        if (this.maxBidstreamLifetimeSeconds == null) {
-            throw new IllegalArgumentException("");
-        }
+        // TODO
+//        if (this.maxBidstreamLifetimeSeconds == null) {
+//            throw new IllegalArgumentException("");
+//        }
         
 //        if (areValuesNull(maxBidstreamLifetimeSeconds, identityTokenExpiresAfterSeconds)) {
 //            logger.error(VALUES_ARE_NULL + MaxBidstreamLifetimeSecondsProp + ", " + IDENTITY_TOKEN_EXPIRES_AFTER_SECONDS);
 //            return false;
 //        }
-        if (this.maxBidstreamLifetimeSeconds < this.identityTokenExpiresAfterSeconds) {
+        if (this.maxBidstreamLifetimeSeconds != null && this.maxBidstreamLifetimeSeconds < this.identityTokenExpiresAfterSeconds) {
             throw new IllegalArgumentException("");
 //            logger.error(MaxBidstreamLifetimeSecondsProp + " ({}) < " + IDENTITY_TOKEN_EXPIRES_AFTER_SECONDS + " ({})", maxBidstreamLifetimeSeconds, identityTokenExpiresAfterSeconds);
         }
@@ -113,44 +113,51 @@ public class RuntimeConfig {
     
     @JsonPOJOBuilder
     @JsonIgnoreProperties(ignoreUnknown = true)
+    @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
     public static class Builder {
+        @JsonProperty
         private Integer identityTokenExpiresAfterSeconds;
+        @JsonProperty
         private Integer refreshTokenExpiresAfterSeconds;
+        @JsonProperty
         private Integer refreshIdentityTokenAfterSeconds;
+        @JsonProperty
         private Integer sharingTokenExpirySeconds;
+        @JsonProperty
         private Integer maxBidstreamLifetimeSeconds;
+        @JsonProperty
         private Integer maxSharingLifetimeSeconds;
 
-        public Builder withIdentityTokenExpiresAfterSeconds(@JsonProperty("identity_token_expires_after_seconds") Integer identityTokenExpiresAfterSeconds) {
+        public Builder withIdentityTokenExpiresAfterSeconds(Integer identityTokenExpiresAfterSeconds) {
             this.identityTokenExpiresAfterSeconds = identityTokenExpiresAfterSeconds;
             return this;
         }
-        
-        public Builder withRefreshTokenExpiresAfterSeconds(@JsonProperty("refresh_token_expires_after_seconds") Integer refreshTokenExpiresAfterSeconds) {
+
+        public Builder withRefreshTokenExpiresAfterSeconds(Integer refreshTokenExpiresAfterSeconds) {
             this.refreshTokenExpiresAfterSeconds = refreshTokenExpiresAfterSeconds;
             return this;
         }
-        
-        public Builder withRefreshIdentityTokenAfterSeconds(@JsonProperty("refresh_identity_token_after_seconds") Integer refreshIdentityTokenAfterSeconds) {
+
+        public Builder withRefreshIdentityTokenAfterSeconds(Integer refreshIdentityTokenAfterSeconds) {
             this.refreshIdentityTokenAfterSeconds = refreshIdentityTokenAfterSeconds;
             return this;
         }
 
-        public Builder withSharingTokenExpirySeconds(@JsonProperty("sharing_token_expiry_seconds") Integer sharingTokenExpirySeconds) {
+        public Builder withSharingTokenExpirySeconds(Integer sharingTokenExpirySeconds) {
             this.sharingTokenExpirySeconds = sharingTokenExpirySeconds;
             return this;
         }
 
-        public Builder withMaxBidstreamLifetimeSeconds(@JsonProperty("max_bidstream_lifetime_seconds") Integer maxBidstreamLifetimeSeconds) {
+        public Builder withMaxBidstreamLifetimeSeconds(Integer maxBidstreamLifetimeSeconds) {
             this.maxBidstreamLifetimeSeconds = maxBidstreamLifetimeSeconds;
             return this;
         }
 
-        public Builder withMaxSharingLifetimeSeconds(@JsonProperty("max_sharing_lifetime_seconds") Integer maxSharingLifetimeSeconds) {
+        public Builder withMaxSharingLifetimeSeconds(Integer maxSharingLifetimeSeconds) {
             this.maxSharingLifetimeSeconds = maxSharingLifetimeSeconds;
             return this;
         }
-        
+
         public RuntimeConfig build() {
             return new RuntimeConfig(this);
         }

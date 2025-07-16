16e4cf748425fd4b4e2c0a1dd560bc8871464d73
Katherine Chen <katherine.chen@thetradedesk.com>
2025-07-16 13:38:34 +1000

    Log participant in GenericFailureHandler

--- src/main/java/com/uid2/operator/vertx/GenericFailureHandler.java
+++ src/main/java/com/uid2/operator/vertx/GenericFailureHandler.java
@@ -1,5 +1,8 @@
 package com.uid2.operator.vertx;
 
+import com.uid2.shared.auth.IAuthorizable;
+import com.uid2.shared.auth.OperatorKey;
+import com.uid2.shared.middleware.AuthMiddleware;
 import io.vertx.core.Handler;
 import io.vertx.core.http.HttpClosedException;
 import io.vertx.core.http.HttpServerResponse;
@@ -19,16 +22,23 @@ public class GenericFailureHandler implements Handler<RoutingContext> {
         String url = ctx.normalizedPath();
         Throwable t = ctx.failure();
 
+        final IAuthorizable profile = AuthMiddleware.getAuthClient(ctx);
+        final OperatorKey operatorKey = profile instanceof OperatorKey ? (OperatorKey) profile : null;
+        String participant = "unknown";
+        if (operatorKey != null) {
+            participant = operatorKey.getName();
+        }
+
         if (t != null) {
             // Because Vert.x swallows stack traces so cannot log stack trace
             // And we want to ignore HttpClosedException errors as it is (usually) caused by users and no impact
             if (t instanceof HttpClosedException) {
-                LOGGER.warn("Ignoring exception - URL: [{}] - Error:", url, t);
+                LOGGER.warn("Ignoring exception - URL: [{}], Participant: [{}] - Error:", url, participant, t);
                 response.end();
             } else if (statusCode >= 500 && statusCode < 600) { // 5xx is server error, so error
-                LOGGER.error("URL: [{}] - Error response code: [{}] - Error:", url, statusCode, t);
+                LOGGER.error("URL: [{}], Participant: [{}] - Error response code: [{}] - Error:", url, participant, statusCode, t);
             } else if (statusCode >= 400 && statusCode < 500) { // 4xx is user error, so just warn
-                LOGGER.warn("URL: [{}] - Error response code: [{}] - Error:", url, statusCode, t);
+                LOGGER.warn("URL: [{}], Participant: [{}] - Error response code: [{}] - Error:", url, participant, statusCode, t);
             }
         }
 

90dabf8e5f27070cc4b3556df5c5045c3b7116c5
way zheng <way.zheng@thetradedesk.com>
2025-10-10 12:25:36 -0700

    clean up

--- src/main/java/com/uid2/operator/Main.java
+++ src/main/java/com/uid2/operator/Main.java
@@ -100,8 +100,7 @@ public class Main {
         this.config = config;
 
         this.appVersion = ApplicationVersion.load("uid2-operator", "uid2-shared", "uid2-attestation-api");
-        HealthManager.instance.registerGenericComponent(
-                new PodTerminationMonitor(config.getInteger(Const.Config.PodTerminationCheckInterval, 3000)));
+        HealthManager.instance.registerGenericComponent(new PodTerminationMonitor(config.getInteger(Const.Config.PodTerminationCheckInterval, 3000)));
         // allow to switch between in-mem optout file cache and on-disk file cache
         if (config.getBoolean(Const.Config.OptOutInMemCacheProp)) {
             this.fsLocal = new MemCachedStorage();

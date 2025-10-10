b3929cc1fc5e922a53e177dbe7c5887fb7c4c747
way zheng <way.zheng@thetradedesk.com>
2025-10-10 12:29:40 -0700

    clean up

--- src/main/java/com/uid2/operator/Main.java
+++ src/main/java/com/uid2/operator/Main.java
@@ -101,7 +101,7 @@ public class Main {
 
         this.appVersion = ApplicationVersion.load("uid2-operator", "uid2-shared", "uid2-attestation-api");
         HealthManager.instance.registerGenericComponent(new PodTerminationMonitor(config.getInteger(Const.Config.PodTerminationCheckInterval, 3000)));
-        
+
         // allow to switch between in-mem optout file cache and on-disk file cache
         if (config.getBoolean(Const.Config.OptOutInMemCacheProp)) {
             this.fsLocal = new MemCachedStorage();
@@ -157,12 +157,12 @@ public class Main {
                     new GlobalScope(new CloudPath(cloudEncryptionKeyMdPath)));
 
             String keypairMdPath = this.config.getString(Const.Config.ClientSideKeypairsMetadataPathProp);
-            this.clientSideKeypairProvider = new RotatingClientSideKeypairStore(fsStores,
-                    new GlobalScope(new CloudPath(keypairMdPath)), cloudEncryptionKeyProvider);
+            this.clientKeyProvider = new RotatingClientKeyProvider(fsStores, new GlobalScope(new CloudPath(clientsMdPath)),
+                cloudEncryptionKeyProvider);
             String clientsMdPath = this.config.getString(Const.Config.ClientsMetadataPathProp);
             this.clientKeyProvider = new RotatingClientKeyProvider(fsStores,
                     new GlobalScope(new CloudPath(clientsMdPath)),
-                    cloudEncryptionKeyProvider);
+                cloudEncryptionKeyProvider);
             String keysetKeysMdPath = this.config.getString(Const.Config.KeysetKeysMetadataPathProp);
             this.keysetKeyStore = new RotatingKeysetKeyStore(fsStores, new GlobalScope(new CloudPath(keysetKeysMdPath)),
                     cloudEncryptionKeyProvider);
